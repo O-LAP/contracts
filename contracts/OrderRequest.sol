84e7cee9d48a659646572242caa2d0fe47f7424d
amitlzkpa <amit.lzkpa@gmail.com>
2019-09-22 21:33:03 -0400

    adding unknown changes;check later if it has to be reverted

--- contracts/OrderRequest.sol
+++ contracts/OrderRequest.sol
@@ -9,107 +9,26 @@ contract OrderRequest {
     
     // ------
     
+    
+    uint256 public id;
+    uint public code;
+    
+    
+    // ------
+
 
     address public owner;
-    int256 public destination;
-    bool public completed = false;
-    uint public amount;
-    uint256 public deadline;
+    int256 public delivery_address;
     bytes32 public message;
-    address public assigned_to;
-    mapping(address => uint) public bids;
-    address[] public bidders;
-    uint public request_security = 0.042 ether;
-    uint public bid_security = 0.007 ether;
-
-
-    function getBidCount() public constant returns(uint count) {
-        return bidders.length;
-    }
+    uint256 public deadline;
+    bytes32 public data;
     
     
-    // ------
-
-
-    event Init(uint256 deadline, int256 destination, uint256 amount, bytes32 message);
-    event Bid(address bidder);
-    event AssignedTo(address assignee);
-    event MarkedComplete(uint amount);
-    event Claimed(address claimant, uint amount);
-    
-    function OrderRequest(uint256 deadln) public {
-        require(now < deadln);
-        owner = msg.sender;
-        deadline = deadln;
-        completed = false;
-    }
-    
-    
-    // ------
-
-
-    function init(uint amt, int256 dst, bytes32 mssg) public payable {
-        require(msg.value >= amt+request_security);
-        destination = dst;
-        amount = amt;
-        message = mssg;
-        emit Init(deadline, destination, amount, mssg);
-    }
-    
-    
-    function bid() public payable {
-        require(now < deadline);
-        require(this.balance >= amount+request_security);
-        require(msg.sender != owner);
-        require(assigned_to == 0);
-        require(!completed);
-        require(msg.value == bid_security);
-        require(bids[msg.sender] == 0);
-        bids[msg.sender] += msg.value;
-        bidders.push(msg.sender);
-        emit Bid(msg.sender);
-    }
-
-    function assign(address assignee) public {
-        require(msg.sender == owner);
-        require(this.balance >= amount+request_security);
-        require(now < deadline);
-        require(!completed);
-        require(bids[assignee] != 0);
-        assigned_to = assignee;
-        emit AssignedTo(assignee);
-    }
-    
-    function mark_complete() public {
-        require(msg.sender == owner);
-        require(this.balance >= amount+request_security);
-        require(!completed);
-        require(assigned_to != 0);
-        completed = true;
-        bids[assigned_to] += amount;
-        msg.sender.transfer(bid_security);
-        emit MarkedComplete(amount);
-    }
-
-    function claim() public {
-        if(now > deadline) {
-            msg.sender.transfer(bids[msg.sender]);
-            emit Claimed(msg.sender, bids[msg.sender]);
-            return;
-        }
-        assert(completed);
-        assert(bids[msg.sender] != 0);
-        assert(this.balance >= bids[msg.sender]);
-        msg.sender.transfer(bids[msg.sender]);
-        emit Claimed(msg.sender, bids[msg.sender]);
-    }
-
-
-
     // ------
     
     
     // ------
 
+    
 
 }
